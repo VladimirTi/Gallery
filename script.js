2d4fdac7851e18d2509dacd7659374b643d1a4dc
VladimirTi <tytmes@mail.ru>
2019-05-21 20:34:18 +0300

    Vladimir's Solution

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('#thumbs').addEventListener('click', (event) => {
+  event.preventDefault();
+  const targetElement = event.target
+  const targetSrc = targetElement.getAttribute('src')
+  const largeImg = document.querySelector('#largeImg')
+
+  if(targetSrc) {
+  largeImg.setAttribute('src', targetElement.getAttribute('src'));
+  largeImg.setAttribute('alt', targetElement.parentElement.getAttribute('title'))
+  }
+})
