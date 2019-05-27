c05029846d5abce26031f42715b0d2c078042bdf
VladimirTi <tytmes@mail.ru>
2019-05-27 20:25:59 +0300

    fs_apr19_volodymyr_t

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.querySelector('#thumbs').addEventListener('click', (event) => {
   event.preventDefault();
-  const targetElement = event.target
-  const targetSrc = targetElement.getAttribute('src')
-  const largeImg = document.querySelector('#largeImg')
+  const targetElement = event.target;
+  const targetSrc = targetElement.getAttribute('src');
+  const largeImg = document.querySelector('#largeImg');
 
-  if(targetSrc) {
-  largeImg.setAttribute('src', targetElement.getAttribute('src'));
-  largeImg.setAttribute('alt', targetElement.parentElement.getAttribute('title'))
+  if (targetSrc) {
+    largeImg.setAttribute('src', targetSrc);
+    largeImg.setAttribute('alt', targetElement.parentElement.getAttribute('title'));
   }
-})
+});
